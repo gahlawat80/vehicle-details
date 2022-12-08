75c63bc448ec4a99a1b5cbc598c554c2be0bfda8
gahla <gahlawat80@gmail.com>
2022-12-08 15:22:09 -0500

    intial commit

---
+++ src/main/java/com/learn2code/api/vehicle/details/VehicleDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.learn2code.api.vehicle.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn2code/api/vehicle/details/VehicleDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.api.vehicle.details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
