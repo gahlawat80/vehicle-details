0ac6a56e99b071786b4c76185024b11f9fa50673
gahla <gahlawat80@gmail.com>
2022-12-10 18:36:42 -0500

    create get all vehicle details api

--- src/main/java/com/learn2code/api/vehicle/details/service/impl/VehicleDetailServiceImpl.java
+++ src/main/java/com/learn2code/api/vehicle/details/service/impl/VehicleDetailServiceImpl.java
@@ -2,10 +2,13 @@ package com.learn2code.api.vehicle.details.service.impl;
 
 import com.learn2code.api.vehicle.details.dao.VehicleDetailsDAO;
 import com.learn2code.api.vehicle.details.entities.VehicleDetail;
+import com.learn2code.api.vehicle.details.errors.VehicleDetailsNotFound;
 import com.learn2code.api.vehicle.details.errors.VehicleNotSaved;
 import com.learn2code.api.vehicle.details.service.VehicleDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleDetailServiceImpl implements VehicleDetailService {
@@ -21,5 +24,14 @@ public class VehicleDetailServiceImpl implements VehicleDetailService {
             throw new VehicleNotSaved("Unable to save vehicle in DB. Got error:- "+e.getMessage());
         }
         return dbVehicle;
+    }
+
+    @Override
+    public List<VehicleDetail> fetchAllVehicleDetails() throws VehicleDetailsNotFound {
+        List<VehicleDetail> dbVehicles = vehicleDetailsDAO.findAll();
+        if(dbVehicles.size()==0){
+            throw new VehicleDetailsNotFound("No vehicle details found in Database!");
+        }
+        return dbVehicles;
     }
 }
